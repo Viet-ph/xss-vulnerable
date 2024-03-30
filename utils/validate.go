afa7f982a1dfcbae1d31c8ad129d5074f81e9388
Viet-ph <phviet2910@gmail.com>
2024-03-30 07:38:26 +0700

    Added DOM-Based XSS vulnerbility

--- utils/validate.go
+++ utils/validate.go
@@ -1,48 +1,15 @@
 package utils
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
 	"strings"
 )
 
-func CommentValidateHandler(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		Body string `json:"body"`
-	}
-	type returnCleanedBody struct {
-		CleanedBody string `json:"cleaned_body"`
-	}
+func CommentValidateHandler(original string) string {
 
-	decoder := json.NewDecoder(r.Body)
-	params := parameters{}
-	err := decoder.Decode(&params)
-
-	if err != nil {
-		// an error will be thrown if the JSON is invalid or has the wrong types
-		// any missing fields will simply have their values in the struct set to their zero value
-		fmt.Printf("Error decoding parameters: %s", err)
-		w.WriteHeader(500)
-		return
+	blackList := []string{"<script", "script>", "eval(", "eval&#40;", "javascript:", "javascript&#58;", "fromCharCode", "&#62;", "&#60;", "&lt;", "&rt;"}
+	for _, word := range blackList {
+		original = strings.Replace(original, word, "", -1)
 	}
 
-	if len(params.Body) > 140 {
-		RespondWithError(w, http.StatusBadRequest, "Comment is too long")
-		return
-	}
-
-	profaneWords := []string{"kerfuffle", "sharbert", "fornax"}
-	bodyDat := params.Body
-	bodyDatSplit := strings.Split(bodyDat, " ")
-	for _, profane := range profaneWords {
-		for i, word := range bodyDatSplit {
-			if strings.ToLower(word) == profane {
-				bodyDatSplit[i] = "****"
-			}
-		}
-	}
-
-	bodyDat = strings.Join(bodyDatSplit, " ")
-	RespondWithJSON(w, http.StatusOK, returnCleanedBody{CleanedBody: bodyDat})
+	return original
 }
